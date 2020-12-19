d69ddbdb5513454feeabf498519b577017689de6
Chris Wein <chriswein12@yahoo.com>
2020-12-19 06:34:28 -0600

    added login session

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -16,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
