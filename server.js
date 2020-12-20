9002f3d08a669c85c53cbfcc11fb27d9cea04696
Chris Wein <chriswein12@yahoo.com>
2020-12-19 18:39:13 -0600

    added tests and formatting

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -17,7 +18,7 @@ const sess = {
 };
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 
 const app = express();
